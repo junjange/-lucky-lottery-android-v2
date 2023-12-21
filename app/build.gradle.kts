36bbd4fcfa9c93e4641b370539baba095240f2f0
JunJangE <wnswkd486@gmail.com>
2023-12-21 11:41:19 +0900

    chore: targetSdk 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.junjange.lotto3"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.junjange.lotto3"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
