6550237eb867c7943a5cfc1a49f4871209eeb29a
JunJangE <wnswkd486@gmail.com>
2024-01-03 17:13:19 +0900

    feat: 당첨 번호 뷰 구현

--- presentation/src/main/java/com/junjange/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/junjange/presentation/ui/home/HomeScreen.kt
@@ -1,13 +1,48 @@
 package com.junjange.presentation.ui.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.junjange.presentation.R
+import com.junjange.presentation.component.LottoContent
+import com.junjange.presentation.component.LottoHomeTopBar
+import com.junjange.presentation.ui.theme.LottoTheme
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
 
-    Column {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LottoHomeTopBar(onActionClick = {})
+            LottoContent()
+        }
 
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 15.dp, end = 15.dp),
+            containerColor = LottoTheme.colors.lottoGreen,
+            onClick = { },
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_qr_code),
+                contentDescription = null,
+                tint = LottoTheme.colors.white
+            )
+        }
     }
 }
