545e425af5b9e512de1e7d7c54ee83930f49671a
JunJangE <wnswkd486@gmail.com>
2024-01-10 17:35:46 +0900

    chore: 카카오 키 값 설정

--- app/src/main/java/com/junjange/lotto3/App.kt
+++ app/src/main/java/com/junjange/lotto3/App.kt
@@ -1,11 +1,13 @@
 package com.junjange.lotto3
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this,BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
 }
