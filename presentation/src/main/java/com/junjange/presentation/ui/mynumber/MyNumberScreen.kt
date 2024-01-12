f11b4d0cc4803e9b3fb4fbe9c9eb3bdb9fe55f77
JunJangE <wnswkd486@gmail.com>
2024-01-13 03:06:10 +0900

    feat: ocr을 통해 로또 번호 추가

--- presentation/src/main/java/com/junjange/presentation/ui/mynumber/MyNumberScreen.kt
+++ presentation/src/main/java/com/junjange/presentation/ui/mynumber/MyNumberScreen.kt
@@ -1,5 +1,8 @@
 package com.junjange.presentation.ui.mynumber
 
+import android.graphics.Color
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
@@ -28,32 +31,81 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+import com.canhub.cropper.CropImageView
 import com.junjange.presentation.R
 import com.junjange.presentation.component.Lotto645Content
 import com.junjange.presentation.component.Lotto720Content
 import com.junjange.presentation.component.LottoContentTitle
 import com.junjange.presentation.component.LottoType
+import com.junjange.presentation.ui.mynumber.MyNumberEffect.NavigateToGallery
 import com.junjange.presentation.ui.theme.LottoTheme
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MyNumberScreen(viewModel: MyNumberViewModel = hiltViewModel()) {
+
+    val context = LocalContext.current
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    val imageCropLauncher = rememberLauncherForActivityResult(CropImageContract()) { result ->
+        if (result.isSuccessful) {
+            result.uriContent?.let {
+                viewModel.getTextOfImage(result.getUriFilePath(context, false)!!)
+            }
+        }
+    }
+
+    val imageCropperOptions = CropImageOptions(
+        cropShape = CropImageView.CropShape.RECTANGLE,
+        fixAspectRatio = false,
+        aspectRatioX = 1,
+        aspectRatioY = 1,
+        toolbarColor = Color.WHITE,
+        toolbarBackButtonColor = Color.BLACK,
+        toolbarTintColor = Color.BLACK,
+        allowFlipping = false,
+        allowRotation = false,
+        cropMenuCropButtonTitle = context.getString(R.string.done),
+        imageSourceIncludeCamera = false
+    )
+
+    val imagePickerLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
+            val cropOptions = CropImageContractOptions(uri, imageCropperOptions)
+            imageCropLauncher.launch(cropOptions)
+        }
+
+    LaunchedEffect(viewModel.effect) {
+        viewModel.effect.collectLatest { effect ->
+            when (effect) {
+                is NavigateToGallery -> imagePickerLauncher.launch("image/*")
+            }
+        }
+    }
+
     val tabs = listOf(R.string.lotto_645_title, R.string.lotto_720_title)
     val pagerState = rememberPagerState(pageCount = { tabs.size })
     val coroutineScope = rememberCoroutineScope()
@@ -71,21 +123,31 @@ fun MyNumberScreen(viewModel: MyNumberViewModel = hiltViewModel()) {
 
         HorizontalPager(state = pagerState) { page ->
             when (page) {
-                0 -> MyNumberContent(lottoType = LottoType.LOTTO645)
-                1 -> MyNumberContent(lottoType = LottoType.LOTTO720)
+                0 -> MyNumberContent(
+                    lottoType = LottoType.LOTTO645,
+                    uiState = uiState,
+                    viewModel = viewModel
+                )
+
+                1 -> MyNumberContent(
+                    lottoType = LottoType.LOTTO720,
+                    uiState = uiState,
+                    viewModel = viewModel
+                )
             }
         }
     }
 }
 
 @Composable
-fun MyNumberContent(lottoType: LottoType) {
-    // TODO 임시 데이터
-    val subjects = List(9) { "" }
-
+fun MyNumberContent(
+    lottoType: LottoType,
+    uiState: MyNumberState,
+    viewModel: MyNumberViewModel
+) {
     Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
-            items(subjects) {
+            items(uiState.tempList) {
 
                 val title = if (lottoType == LottoType.LOTTO645) {
                     stringResource(R.string.lotto_645_title)
@@ -107,38 +169,34 @@ fun MyNumberContent(lottoType: LottoType) {
                         if (lottoType == LottoType.LOTTO645) Lotto645Content()
                         else Lotto720Content()
                         Spacer(modifier = Modifier.height(8.dp))
-                        MyNumberContentItem(lottoType = lottoType)
+                        MyNumberContentItem(lottoType = lottoType, tempList = it)
                     }
                 }
             }
         }
 
-        // TODO ocr 기술 도입 예정
-//        FloatingActionButton(
-//            modifier = Modifier
-//                .align(Alignment.BottomEnd)
-//                .padding(bottom = 15.dp, end = 15.dp),
-//            containerColor = LottoTheme.colors.lottoGreen,
-//            onClick = { },
-//        ) {
-//            Icon(
-//                imageVector = Icons.Filled.Edit,
-//                contentDescription = null,
-//                tint = LottoTheme.colors.white
-//            )
-//        }
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 15.dp, end = 15.dp),
+            containerColor = LottoTheme.colors.lottoGreen,
+            onClick = { viewModel.onPickedImage() },
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Edit,
+                contentDescription = null,
+                tint = LottoTheme.colors.white
+            )
+        }
     }
 }
 
 @Composable
-fun MyNumberContentItem(lottoType: LottoType) {
-    // TODO 임시 데이터
-    val subjects = List(3) { "" }
-
-    subjects.forEach {
+fun MyNumberContentItem(lottoType: LottoType, tempList: List<List<String>>) {
+    tempList.forEach { tempListItem ->
         Row(Modifier.fillMaxWidth()) {
             TableCell(text = "미발표", weight = 2f)
-            TableCell(lottoType = lottoType, lottoNumber = List(6) { it.toString() }, weight = 8f)
+            TableCell(lottoType = lottoType, lottoNumber = tempListItem, weight = 8f)
         }
     }
 }
@@ -167,7 +225,11 @@ fun RowScope.TableCell(lottoType: LottoType, lottoNumber: List<String>, weight: 
         horizontalArrangement = Arrangement.Center
     ) {
         lottoNumber.forEach {
-            MyLottoBall(isSuccess = false, lottoType = lottoType, lottoTitle = it)
+            MyLottoBall(
+                isSuccess = false,
+                lottoType = lottoType,
+                lottoTitle = it
+            )
             Spacer(modifier = Modifier.width(8.dp))
         }
     }
