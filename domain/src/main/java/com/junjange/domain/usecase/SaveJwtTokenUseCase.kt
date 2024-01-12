4bd296834dcaf5b82d2ee0112700004256ea3884
JunJangE <wnswkd486@gmail.com>
2024-01-12 16:24:38 +0900

    feat: local data usecase 구현

---
+++ domain/src/main/java/com/junjange/domain/usecase/SaveJwtTokenUseCase.kt
@@ -0,0 +1,11 @@
+package com.junjange.domain.usecase
+
+import com.junjange.domain.model.JwtToken
+import com.junjange.domain.repository.LocalRepository
+import javax.inject.Inject
+
+class SaveJwtTokenUseCase @Inject constructor(
+    private val repository: LocalRepository
+) {
+    suspend operator fun invoke(jwtToken: JwtToken) = repository.saveJwtToken(jwtToken = jwtToken)
+}
