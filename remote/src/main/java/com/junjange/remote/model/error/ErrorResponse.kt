9b8ae68dcf4be2f3c7a113ba2b3df2f8bad75077
JunJangE <wnswkd486@gmail.com>
2024-02-05 19:48:36 +0900

    chore: error data format 수정

--- remote/src/main/java/com/junjange/remote/model/error/ErrorResponse.kt
+++ remote/src/main/java/com/junjange/remote/model/error/ErrorResponse.kt
@@ -3,8 +3,7 @@ package com.junjange.remote.model.error
 interface ErrorResponse {
     val success: Boolean
     val status: Int
-    val code: String
     val reason: String
-    val time_stamp: String
+    val timeStamp: String
     val path: String
 }
--- remote/src/main/java/com/junjange/remote/model/error/ErrorResponseImpl.kt
+++ remote/src/main/java/com/junjange/remote/model/error/ErrorResponseImpl.kt
@@ -5,9 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class ErrorResponseImpl(
     @SerializedName("success") override val success: Boolean,
     @SerializedName("status") override val status: Int,
-    @SerializedName("code") override val code: String,
     @SerializedName("reason") override val reason: String,
-    @SerializedName("time_stamp") override val time_stamp: String,
+    @SerializedName("timeStamp") override val timeStamp: String,
     @SerializedName("path") override val path: String
 ) : ErrorResponse
 
