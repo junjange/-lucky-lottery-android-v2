14e2afa0856f10d1913ea7c7255a5aaabf92ea4b
JunJangE <wnswkd486@gmail.com>
2024-02-06 16:14:18 +0900

    feat: 회원가입 페이지 UI

--- presentation/src/main/java/com/junjange/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/junjange/presentation/ui/main/MainActivity.kt
@@ -83,6 +83,7 @@ class MainActivity : BaseActivity() {
     companion object {
         fun startActivity(context: Context) {
             val intent = Intent(context, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             context.startActivity(intent)
         }
     }
