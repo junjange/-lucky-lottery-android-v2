e16cb6ff05c00acc4f9f44fc8e6454649abb0ac9
JunJangE <wnswkd486@gmail.com>
2024-02-26 17:19:02 +0900

    feat: App MobileAds 초기화

--- app/src/main/java/com/junjange/lotto3/App.kt
+++ app/src/main/java/com/junjange/lotto3/App.kt
@@ -1,6 +1,7 @@
 package com.junjange.lotto3
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -8,6 +9,7 @@ import dagger.hilt.android.HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        KakaoSdk.init(this,BuildConfig.KAKAO_NATIVE_APP_KEY)
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        MobileAds.initialize(this)
     }
 }
