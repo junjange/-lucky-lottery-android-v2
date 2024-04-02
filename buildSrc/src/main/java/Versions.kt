fc413045e813666cfa9cc07a3e95981e48f39131
JunJangE <wnswkd486@gmail.com>
2024-04-03 00:58:36 +0900

    chore: 버전 업데이트

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 24
 
-    const val VERSION_CODE = 4
+    const val VERSION_CODE = 5
     const val VERSION_NAME = "2.0.0"
 }
